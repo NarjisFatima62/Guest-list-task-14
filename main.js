5f1b9b76301233f6952bed7222ce00603fc3a162
Narjis Fatima <161502591+NarjisFatima62@users.noreply.github.com>
2024-05-11 21:59:48 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+"use strict";
+//Question 14:
+//Guest list: zzif you can invite anyone, living or deceased, to dinner, who would you invite? Make a listthat include atleast three poeple you'd like to invite to dinner. Then use your list to print a message to each person, inviting them to dinner.
+//Answer:
+let GuestArr = ["Sara", "Dua", "Zia"];
+GuestArr.map((items) => (console.log(`Dear ${items},you are invited to dinner`)));
